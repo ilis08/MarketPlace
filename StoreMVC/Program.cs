533392b849936e16980052cef038989c96d850bf
ilis08 <nankoviliya@gmail.com>
2022-06-14 11:03:23 +0300

    Startup class in StoreMVC project was replaced with Program

--- StoreMVC/Program.cs
+++ StoreMVC/Program.cs
@@ -1,26 +1,75 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using StoreMVC.Models.Order.SessionCart;
+using StoreMVC.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http.Headers;
 
-namespace StoreMVC
+namespace WebAPI;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+        builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(opts =>
+        {
+            opts.IdleTimeout = TimeSpan.FromDays(120);
+            opts.Cookie.IsEssential = true;
+        });
+
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        builder.Services.AddTransient(c => CartSession.GetCart(c));
+        builder.Services.AddScoped<IProductService, ProductService>();
+
+        builder.Services.AddHttpClient("myapi", c =>
+        {
+            c.BaseAddress = new Uri(builder.Configuration.GetSection("apiPath").Value);
+            c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        });
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseSession();
+
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            endpoints.MapDefaultControllerRoute();
+
+            endpoints.MapRazorPages();
+        });
+
+        app.Run();
     }
 }
---
+++ WebAPI/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using FilesService;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetGreetMessage(string name)
+        {
+            var channel = GrpcChannel.ForAddress(new Uri("https://localhost:7035"));
+            var client = new Greeter.GreeterClient(channel);
+
+            using var result = client.SendMessage();
+
+            List<string> number = new List<string>();
+
+            try
+            {
+                await foreach (var item in result.ResponseStream.ReadAllAsync(CancellationToken.None))
+                {
+                    number.Add(item.Text);
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                throw;
+            }
+
+            return Ok(number);
+        }
+    }
+}
